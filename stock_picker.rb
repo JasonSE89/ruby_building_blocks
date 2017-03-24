567555d7a025976f6d4913080fde497375e75ef9
Jason <jason.epstein89@gmail.com>
2017-03-23 21:08:38 -0400

    stock_picker completed

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,20 +1,27 @@
 def stock_picker(stock_list)
-  current_value = [0,0]
-  stock_list = stock_list.split(' ')
-  new_stock = []
-  stock_list.each do |stock|
-    new_stock.push(stock.to_i)
+ current_value = [0,0]
+ i = 0
+ stock_list = stock_list.split(',')
+ new_stock = []
+ stock_list.each do |stock|
+  new_stock.push(stock.to_i)
+ end
+ if(new_stock[0]==new_stock.max)
+   new_stock.delete_at(0)
+ end
+ if(new_stock[new_stock.length-1]==new_stock.min)
+   new_stock.delete_at(new_stock.length-1)
+ end
+ until i == new_stock.length - 1
+   temp = new_stock.slice(i+1, new_stock.length-1)
+     if(temp.max-new_stock[i]>current_value[1]-current_value[0])
+        current_value[0]=new_stock[i]
+        current_value[1]=temp.max
+       end
+       i+=1
   end
-    until new_stock.length==0
-      if(new_stock.first==new_stock.max)
-      	new_stock.delete(0)
-      elsif(new_stock.last==new_stock.min)
-        new_stock.delete(new_stock.last)
-      else
-        if((current_value.last-current_value.first)<current	
-    end
-end
-
+  puts current_value
+ end
 puts "Enter stock List"
 user_input = gets.chomp
 stock_picker(user_input)
